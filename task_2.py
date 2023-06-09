7adaf6e8c0a41dd5d68e1a97dffb780229e18e92
Daria_Yakymova <kiritocun.ru@gmail.com>
2023-06-09 18:04:05 +0300

    added task 2 final version

--- task_2.py
+++ task_2.py
@@ -18,24 +18,27 @@ list_2 = []
 list_3 = []
 list_4 = []
 list_5 = []
+try:
 
+    for i in range(NUMS_SIZE):
+        nums_list.append(random.randint(FIRST, LAST))
 
-for i in range(NUMS_SIZE):
-    nums_list.append(random.randint(FIRST, LAST))
+    for i in nums_list:
+        if i % 2 == 0:
+            list_2.append(i)
+        if i % 2 != 0:
+            list_3.append(i)
+        if i < 0:
+            list_4.append(i)
+        if i > 0:
+            list_5.append(i)
+    print(nums_list)
+    print(list_2)
+    print(list_3)
+    print(list_4)
+    print(list_5)
 
-for i in nums_list:
-    if i % 2 == 0:
-        list_2.append(i)
-    if i % 2 != 0:
-        list_3.append(i)
-    if i < 0:
-        list_4.append(i)
-    if i > 0:
-        list_5.append(i)
-print(nums_list)
-print(list_2)
-print(list_3)
-print(list_4)
-print(list_5)
+except Exception as error:
+    print(error)
 
 
