dd0144a79752b9ac12204d3fbd8a256a8e3a9f6f
Daria_Yakymova <kiritocun.ru@gmail.com>
2023-06-10 21:44:23 +0300

    added task 1 fixed

--- task_1.py
+++ task_1.py
@@ -18,8 +18,9 @@ minus_num = 0
 even_num = 0 #чет
 odd_num = 0 #нечет
 prod_three = 1
-prod_minmax = 0
+prod_minmax = 1
 sum_fl = 0
+ind_list = []
 try:
     LIST_SIZE = int(input("Enter list size: "))
 
@@ -30,8 +31,8 @@ try:
     START = int(input("Enter first number: "))
     END = int(input("Enter last number: "))
 
-    # if START < END:
-    #     raise ValueError("End should be more than start")
+    if START > END:
+        START, END = END, START
 
     for i in range(LIST_SIZE):
         user_list.append(random.randint(START, END))
@@ -48,9 +49,8 @@ try:
         elif i%2 != 0:
             odd_num += i
 
-    for i in user_list:
-        if user_list.index(i) % 3 == 0:
-            prod_three = prod_three * i
+    for i in range(0, len(user_list), 3):  # шаг 3 для индексов, в данном случае 0 входит.
+        prod_three *= user_list[i]
 
     min_value = min(user_list)
     max_value = max(user_list)
@@ -59,14 +59,28 @@ try:
     max_ind = user_list.index(max_value)
 
     if min_ind < max_ind:
-        print(user_list[min_ind:max_ind])
+        for i in range(min_ind + 1, max_ind):
+            prod_minmax *= user_list[i]
     else:
-        print(user_list[max_ind + 1:min_ind])
+        for i in range(max_ind + 1, min_ind):
+            prod_minmax *= user_list[i]
+
+    for i in user_list:
+        if i > 0:
+            ind_list.append(user_list.index(i))
+
+    first_pos = ind_list[0]  # indexes
+    last_pos = ind_list[-1]
+
+    for i in range(first_pos + 1, last_pos):
+        sum_fl += user_list[i]
 
     print(f"Sum of minus number: {minus_num}")
     print(f"Sum of even number: {even_num}")
     print(f"Sum of odd number: {odd_num} ")
     print(f"The product of elements with an index divisible by three: {prod_three}")
+    print(f"Product between min and max: {prod_minmax}")
+    print(f"Sum between first and last positive element: {sum_fl}")
 
 except ValueError as error:
     print(error)
